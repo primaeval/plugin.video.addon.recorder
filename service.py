7cf221a4a162e0b23627a429396a33d4e13702cf
primaeval <primaeval@users.noreply.github.com>
2018-11-13 17:51:44 +0100

    service

--- service.py
+++ service.py
@@ -72,61 +72,62 @@ conn.close()
 player_monitor = KodiPlayer()
 
 
-def do_update():
-    global last_update
-    log("do_update")
+
+servicing = False
+
+def Service():
+    global servicing
+    if servicing:
+        return
+    servicing = True
+    xbmc.log("[plugin.video.addon.recorder] SERVICE",xbmc.LOGERROR)
     xbmc.executebuiltin('XBMC.RunPlugin(plugin://plugin.video.addon.recorder/service)')
-    last_update = datetime.datetime.now()
+    time.sleep(2)
+    servicing = False
 
-last_update = datetime.datetime(1970,1,1)
-
-if plugin.get_setting('service',bool) and plugin.get_setting('service.startup',bool):
-    do_update()
-
-monitor = xbmc.Monitor()
-while not monitor.abortRequested():
-    time_left = 10
-    update = False
-    if plugin.get_setting('service',bool):
-        if plugin.get_setting('service.type',int) == 1:
-            interval = plugin.get_setting('service.interval',int)
-            next_update = last_update + interval*10 #3600
-            log(("next_update",next_update))
-            now = time.time()
-            if now > next_update:
-                time_left = 1
-                update = True
-            else:
-                time_left = next_update - now
-        elif plugin.get_setting('service.type',int) == 2:
-            hms = plugin.get_setting('service.time',str).split(':')
-            log(hms)
-            hour = hms[0]
-            minute  = hms[1]
+if plugin.get_setting('service',bool):
+    monitor = xbmc.Monitor()
+    xbmc.log("[plugin.video.addon.recorder] service started...",xbmc.LOGERROR)
+    if plugin.get_setting('service.startup',bool):
+        Service()
+        plugin.set_setting('last.update', str(time.time()))
+    while not monitor.abortRequested():
+        if plugin.get_setting('service.type') == '1':
+            interval = int(plugin.get_setting('service.interval'))
+            waitTime = 3600 * interval
+            ts = plugin.get_setting('last.update') or "0.0"
+            lastTime = datetime.datetime.fromtimestamp(float(ts))
             now = datetime.datetime.now()
-            next_time = now.replace(hour=int(hour),minute=int(minute),second=0,microsecond=0)
-            if next_time < now:
-                next_time = next_time + datetime.timedelta(hours=24)
-            last_time = next_time - datetime.timedelta(hours=24)
-            td = next_time - now
-            time_left = td.seconds + (td.days * 24 * 3600)
-            elapsed = now - last_update
-            since = now-last_time
-            log(("last_time,next_time,since,until",last_time,next_time,now-last_time,next_time-now))
-            log(("now,last_update,elapsed,time_left",now,last_update,elapsed,time_left))
-            if elapsed > datetime.timedelta(hours=24):
-                update = True
-            if since > datetime.timedelta(hours=24):
-                update = True 
-            
+            nextTime = lastTime + datetime.timedelta(seconds=waitTime)
+            td = nextTime - now
+            timeLeft = td.seconds + (td.days * 24 * 3600)
+            xbmc.log("[plugin.video.addon.recorder] Service waiting for interval %s" % waitTime,xbmc.LOGERROR)
+        elif plugin.get_setting('service.type') == '2':
+            next_time = plugin.get_setting('service.time')
+            if next_time:
+                hms = next_time.split(':')
+                hour = hms[0]
+                minute  = hms[1]
+                now = datetime.datetime.now()
+                next_time = now.replace(hour=int(hour),minute=int(minute),second=0,microsecond=0)
+                if next_time < now:
+                    next_time = next_time + datetime.timedelta(hours=24)
+                td = next_time - now
+                timeLeft = td.seconds + (td.days * 24 * 3600)
+        if timeLeft <= 0:
+            timeLeft = 1
+        xbmc.log("[plugin.video.addon.recorder] Service waiting for %d seconds" % timeLeft,xbmc.LOGERROR)
+        if timeLeft and monitor.waitForAbort(timeLeft):
+            break
+        xbmc.log("[plugin.video.addon.recorder] Service now triggered...",xbmc.LOGERROR)
+        Service()
+        now = time.time()
+        plugin.set_setting('last.update', str(now))
+else:
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        time.sleep(5)
 
-    log(("time_left",time_left))
-    if update:
-        if plugin.get_setting('service',bool) and plugin.get_setting('service.type',int) != 0:
-            log("update")
-            do_update()
-    log("tick")
-    time.sleep(5)
 
 
 del player_monitor
